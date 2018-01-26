58e5523227fef6b027eb892f1c0705992ce315f6
Future <mn7697np@gmail.com>
2018-01-26 17:14:44 +0330

    Add Marburg genes

--- Alignment.py
+++ Alignment.py
@@ -1,8 +1,16 @@
 import sys
-
+from Bio import SeqIO
+from Bio import AlignIO
 sys.setrecursionlimit(10000000)
 
 """
     Created by Mohsen Naghipourfar on 1/26/18.
     Email : mn7697np@gmail.com
 """
+
+marburg_genes = []
+for seq_record in SeqIO.parse("./Data/Marburg_Genes.fasta", "fasta"):
+    marburg_genes.append(seq_record)
+
+
+
