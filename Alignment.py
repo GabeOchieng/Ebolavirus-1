6bbaf4ac3c09e1bf99767a4c9bf833cb8d4ee8f1
Future <mn7697np@gmail.com>
2018-01-26 21:22:19 +0330

    Add Ouput Files

--- Alignment.py
+++ Alignment.py
@@ -10,6 +10,24 @@ sys.setrecursionlimit(10000000)
     Email : mn7697np@gmail.com
 """
 
+
+def align_and_find_genes(genome):
+    f = open('./Output/' + genome.name + '.txt', "w")
+    start = 0
+    for gene in marburg_genes:
+        len_gene = len(gene.seq)
+        end = (start + len_gene * 3) if len(genome) > (start + len_gene * 3) else len(genome)
+        gene_str = str(gene.seq)
+        genome_str = str(genome.seq)[start: start + len_gene * 3]
+        alignments = pairwise2.align.localmd(gene_str, genome_str, 1, -1, -1, -1, 0, 0)
+        final_alignment = alignments[0]  # align1, align2, score, begin, end
+        begin_idx = final_alignment[3]
+        end_idx = final_alignment[4]
+        f.write(gene.name + " :" + str(start + begin_idx) + " - " + str(start + end_idx) + "\n")
+        start = end - len_gene
+    f.close()
+
+
 # read Marburg genes data
 marburg_genes = []
 for seq_record in SeqIO.parse("./Data/Marburg_Genes.fasta", "fasta"):
@@ -24,18 +42,6 @@ Zaire_genome = SeqIO.read("./Data/Zaire_genome.fasta", "fasta")
 ebolavirus_genomes = [Bundibugyo_genome, Reston_genome, Sudan_genome, TaiForest_genome, Zaire_genome]
 
 for genome in ebolavirus_genomes:
-    f = open('./Output/' + genome.name + '.txt', "w")
-    start = 0
-    for gene in marburg_genes:
-        len_gene = len(gene.seq)
-        end = (start + len_gene * 3) if len(genome) > (start + len_gene * 3) else len(genome)
-        gene_str = str(gene.seq)
-        genome_str = str(genome.seq)[start: start + len_gene * 3]
-        alignments = pairwise2.align.localmd(gene_str, genome_str, 1, -1, -1, -1, 0, 0)
-        final_alignment = alignments[0]     # align1, align2, score, begin, end
-        begin_idx = final_alignment[3]
-        end_idx = final_alignment[4]
-        f.write(gene.name + " :" + str(start + begin_idx) + " - " + str(start + end_idx) + "\n")
-        start = end - len_gene
-    f.close()
+    align_and_find_genes(genome)
+    break
 
--- EditDistances.py
+++ EditDistances.py
@@ -1,5 +1,7 @@
 import sys
-from Bio.Phylo.TreeConstruction import DistanceCalculatorsys.setrecursionlimit(10000000)
+from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio import AlignIO
+sys.setrecursionlimit(10000000)
 
 """
     Created by Mohsen Naghipourfar on 1/26/18.
